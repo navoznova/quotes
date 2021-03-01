0868b6300fb8a576d648d10da803d1777402f091
Anna Navoznova <anna.navoznova@gmail.com>
2021-03-01 21:18:21 +0300

    Сделала формирвоание списка кнопок-категорий

--- catalog.js
+++ catalog.js
@@ -1,34 +1,40 @@
+import QuoteStorage from './Model/quoteStorage.js'
 export default class Catalog {
-    constructor(quotes) {
-        this.quotes = quotes;
+    constructor() {
+        this.quoteStorage = new QuoteStorage();
+        this.quotes = this.quoteStorage.getAllQuotes();
+    
+        let categories = this.getCategories();
+        let categoriesHtml = categories.map(category => `<li><button>${category}</button></li>`);
+        let categoriesHtmlStr = categoriesHtml.join(''); 
+
         let catalogContainer = document.createElement('ul');
         catalogContainer.id = "catalog";
-
-        // TODO: избавиться от хардкода. Составлять список копок на основе списка всех категорий из QuoteStorage
-        let categoriasListHtml = `
-            <li><button data-category=all>ВСЕ</button></li>
-            <li><button data-category=economy>Экономика</button></li>
-            <li><button data-category=politics>Политика</button></li>
-            <li><button data-category=culture>Культура</button></li>`;
-
-        catalogContainer.innerHTML = categoriasListHtml;
+        catalogContainer.innerHTML = categoriesHtmlStr;
         catalogContainer.addEventListener('click', (event) => { this.showQuoteButtonClickHandler(event) });
 
         this.htmlElement = catalogContainer;
     }
 
+    getCategories() {
+        let categories = this.quotes.map(quote => quote.category);
+        return [...new Set(categories)];
+    }
+    
     showQuoteButtonClickHandler(event) {
-        const category = event.target.dataset.category;
-
-        let quotesByCategories = this.quotes.filter(quote => quote.category === category);
+        let quotesByCategories = this.quotes.filter(quote => quote.category === event.target.textContent);
         let randomIndex = Math.floor(Math.random() * quotesByCategories.length);
         const randomQuote = quotesByCategories[randomIndex];
 
         function getQuoteHtml(quote){
-            return quote.text + ' ' + quote.author;
+            return `${quote.text} ${quote.author}`;
         }
     
         document.getElementById('output').innerHTML = getQuoteHtml(randomQuote);
     }    
+    
+    addQuote(quote) {
+        this.quoteStorage.addQuote(quote);
+    }
 }
 
