751430a0eb087958ab30d4f2c48a697895112663
Anna Navoznova <anna.navoznova@gmail.com>
2021-03-09 12:59:14 +0300

    Создала класс quoteCart, добавила обработчик на кнопку Save

---
+++ Model/quoteCart.js
@@ -0,0 +1,8 @@
+export default class QuoteCart {
+    constructor(categoryName, quoteText, authorName, tagName){
+        this.category = categoryName;
+        this.text = quoteText;
+        this.author = authorName;
+        this.tag = tagName;
+    }
+}
--- Model/quoteStorage.js
+++ Model/quoteStorage.js
@@ -1,4 +1,6 @@
 export default class QuoteStorage {
+    constructor() {
+    }
     getAllQuotes() {
         let jsonStr = localStorage['quotes'];
         return JSON.parse(jsonStr);
@@ -11,3 +13,44 @@ export default class QuoteStorage {
         localStorage['quotes'] = jsonStr;
     }
 }
+
+/*        quotes = [
+            {
+                category: 'politics',
+                title: "Политика",
+                text: "Быть нацией по сути самая универсальная легитимная ценность в политической жизни нашего времени",
+                author: "Б. Андерсон",
+            },
+            {
+                category: 'politics',
+                title: "Политика",
+                text: "Мы должны завоевать народы нашей промышленностью и победить их нашим вкусом",
+                author: "Кольбе",
+            },
+            {
+                category: 'economy',
+                title: "Экономика",
+                text: "Не работодатель выдает зарплату, работодатель только распределяет деньги. Зарплату выдает клиент",
+                author: "",
+            },
+            {
+                category: 'economy',
+                title: "Экономика",
+                text: "Роль правительства в свободном обществе делать то, что рынок не может делать для себя сам, а именно определять, устанавливать и поддерживать правила игры",
+                author: "М.  Фридман",
+            },
+            {
+                category: 'culture',
+                title: "Культура",
+                text: "Все носят невидимые очки, стекла которых сварены из услышанных или прочитанных слов, идей и мнений. Никто не свободен от очков",
+                author: "Н. Арутюнян",
+            },
+        ];
+        // TODO: вытаскивать все цитаты из места хранения см https://developer.mozilla.org/ru/docs/Web/API/Window/localStorage
+        return quotes;
+    }
+
+    saveNewQuote(quote) {
+    }
+}*/
+
