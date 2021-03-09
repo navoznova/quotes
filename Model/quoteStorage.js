3da22048a03fb24a5a23bd02061f0b40b8a603e2
Anna Navoznova <anna.navoznova@gmail.com>
2021-03-09 10:35:18 +0300

    Перенесли цитаты в localStorage

--- Model/quoteStorage.js
+++ Model/quoteStorage.js
@@ -1,36 +1,13 @@
 export default class QuoteStorage {
     getAllQuotes() {
-        let quotes = [
-            {
-                category: "Политика",
-                text: "Быть нацией по сути самая универсальная легитимная ценность в политической жизни нашего времени",
-                author: "Б. Андерсон",
-            },
-            {
-                category: "Политика",
-                text: "Мы должны завоевать народы нашей промышленностью и победить их нашим вкусом",
-                author: "Кольбе",
-            },
-            {
-                category: "Экономика",
-                text: "Не работодатель выдает зарплату, работодатель только распределяет деньги. Зарплату выдает клиент",
-                author: "",
-            },
-            {
-                category: "Экономика",
-                text: "Роль правительства в свободном обществе делать то, что рынок не может делать для себя сам, а именно определять, устанавливать и поддерживать правила игры",
-                author: "М.  Фридман",
-            },
-            {
-                category: "Культура",
-                text: "Все носят невидимые очки, стекла которых сварены из услышанных или прочитанных слов, идей и мнений. Никто не свободен от очков",
-                author: "Н. Арутюнян",
-            },
-        ];
-        // TODO: вытаскивать все цитаты из места хранения см https://developer.mozilla.org/ru/docs/Web/API/Window/localStorage
-        return quotes;
+        let jsonStr = localStorage['quotes'];
+        return JSON.parse(jsonStr);
     }
 
     saveNewQuote(quote) {
+        let quotes = this.getAllQuotes();
+        quotes.push(quote);
+        let jsonStr = JSON.stringify(quotes);
+        localStorage['quotes'] = jsonStr;
     }
 }
