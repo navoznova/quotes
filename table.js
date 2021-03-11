e7b50e2357db9c3e9c57eaa3b0a5cdeda2b73ce9
Anna Navoznova <anna.navoznova@gmail.com>
2021-03-11 09:58:32 +0300

    Добавлены методы добавить строку, удалить строку в таблице

--- table.js
+++ table.js
@@ -1,31 +1,37 @@
 import QuoteStorage from './Model/quoteStorage.js'
-
 export default class Table {
 
-    constructor(users) {
-        this.quoteStorage = new QuoteStorage();
-        this.quotes = this.quoteStorage.getAllQuotes();
+    constructor() {
+        let quoteStorage = new QuoteStorage();
+        this.quotes = quoteStorage.getAllQuotes();
         this.fieldNames = ['category', 'text', 'tag', 'country', 'year'];
 
         this.tbody = document.createElement('tbody');
-        let trs = this.quotes.map(quote => this.addRow(quote));
+        let trs = this.quotes.map(quote => this.getRowHtml(quote));
         this.tbody.innerHTML = trs.join('');
 
         let buttons = this.tbody.querySelectorAll('button');
         for (let button of buttons) {
-            button.addEventListener('click', deleteRow);
+            button.addEventListener('click', (event) => this.deleteRow(event));
+            button.addEventListener('click', (event) => quoteStorage.deleteQuote(event));
         }
-
-        function deleteRow(event) {
-            let trToDelete = event.target.closest('tr');
-            trToDelete.remove();
-        };
     }
 
-    addRow(quote) {
+    getRowHtml(quote) {
         let tds = this.fieldNames.map(fieldName => `<td>${quote[fieldName]}</td>`);
         tds.push('<td><button>X</button></td>');
         let tr = `<tr>${tds.join('')}</tr>`;
         return tr;
+    }
+
+    addRow(quote) {
+        let tr = document.createElement('tr');
+        tr.innerHTML = this.getRowHtml(quote);
+        document.querySelector('tbody').appendChild(tr);
+    }
+
+    deleteRow(event) {
+        let trToDelete = event.target.closest('tr');
+        trToDelete.remove();
     }
 }
