b128f841380cb4467d0d949cb7cb59d3dfa0f0d0
Anna Navoznova <anna.navoznova@gmail.com>
2021-03-13 17:36:09 +0300

    Верстка страницы User

--- catalog.js
+++ catalog.js
@@ -5,10 +5,12 @@ export default class Catalog {
         this.quotes = this.quoteStorage.getAllQuotes();
 
         let categories = this.getCategories();
-        let categoryHtmls = categories.map(category => 
-        `<div class="col">
+        let categoryHtmls = categories.map(category => `<div class="col">
           <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>${category}</title>
+            <rect width="100%" height="100%" fill="#55595c"/>
+            <text x="50%" y="50%" fill="#eceeef" dy=".3em">${category}</text></svg>
             <div class="card-body">
             <h5 class="card-title">${category}</h5>
               <p class="card-text">Описание категории</p>
@@ -18,16 +20,16 @@ export default class Catalog {
             </div>
           </div>
         </div>`);
-        categoryHtmls.push('<button class="btn btn-primary my-2" data-name="All">Все</button>')
+        categoryHtmls.push('<button class="btn btn-primary" data-name="All">Все</button>')
         let categoriesHtmlStr = categoryHtmls.join('');
 
         let catalogContainer = document.createElement('div');
         catalogContainer.id = "catalog";
-        catalogContainer.classList.add('row','row-cols-1','row-cols-sm-2','row-cols-md-3','g-3');
+        catalogContainer.classList.add('row', 'row-cols-1', 'row-cols-sm-2', 'row-cols-md-3', 'g-3');
 
         catalogContainer.innerHTML = categoriesHtmlStr;
         catalogContainer.addEventListener('click', (event) => { this.showQuoteButtonClickHandler(event) });
-        this.htmlElement = catalogContainer;        
+        this.htmlElement = catalogContainer;
     }
 
     getCategories() {
