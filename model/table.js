c845fb0acf79ae1ee3b6d9d65111fbef9e18bb75
Anna Navoznova <anna.navoznova@gmail.com>
2021-03-17 12:45:30 +0300

    Вынесла из конструктора в метода код по формированию категорий, убрала дублирующие функции

--- model/table.js
+++ model/table.js
@@ -3,24 +3,18 @@ export default class Table {
     constructor() {
         this.quoteStorage = new QuoteStorage();
         this.quotes = this.quoteStorage.getAllQuotes();
-        this.fieldNames = ['category', 'text', 'tag', 'country', 'year'];
 
         this.tbody = document.createElement('tbody');
         let trHtmls = this.quotes.map(quote => this.getRowHtml(quote));
         this.tbody.innerHTML = trHtmls.join('');
 
         let buttons = this.tbody.querySelectorAll('button');
-        buttons.forEach(button => {
-            button.addEventListener('click', (event) => {
-                let quoteId = event.target.closest('tr').dataset.quoteId;
-                this.deleteRow(quoteId);
-                this.quoteStorage.deleteQuote(quoteId);
-            });
-        });
+        buttons.forEach(button => this.addListener(button));
     }
 
     getRowHtml(quote) {
-        let tdHtmls = this.fieldNames.map(fieldName => {
+        let fieldNames = ['category', 'text', 'tag', 'country', 'year'];
+        let tdHtmls = fieldNames.map(fieldName => {
             if (!quote[fieldName]) {
                 quote[fieldName] = '—';
             }
